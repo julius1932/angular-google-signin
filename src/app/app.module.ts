33496fd13bc1b0b4aad2558a444e512e749fc57e
DESKTOP-SF1BBLJ\Dynaton Data Zim <lasjulius@cdp.dental>
2023-10-23 09:01:31 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { SocialLoginModule, SocialAuthServiceConfig, GoogleLoginProvider ,FacebookLoginProvider} from 'angularx-social-login';
 
+
+
 import { AppComponent } from './app.component';
 import { GoogleSigninService } from './google-signin.service';
+
 
 @NgModule({
   declarations: [
@@ -22,7 +25,7 @@ import { GoogleSigninService } from './google-signin.service';
         providers: [
           {
             id: GoogleLoginProvider.PROVIDER_ID,
-            provider: new GoogleLoginProvider('504443391624-53ugeq2o9khhf36s3j3rh2b6a721gsdg.apps.googleusercontent.com')
+            provider: new GoogleLoginProvider('504443391624-j8kbdlmb026ebefbptv4u4q4va2idlhl.apps.googleusercontent.com')
           },
           {
             id: FacebookLoginProvider.PROVIDER_ID,
